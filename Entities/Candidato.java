6f34ca7a1970ee9741e3fc662888e91cfc3bc772
Gabriel Moneta <167481069+bielmoneta@users.noreply.github.com>
2024-10-15 18:41:01 -0300

    first commit

---
+++ Entities/Candidato.java
@@ -0,0 +1,45 @@
+package entidades;
+
+public class Candidato {
+	
+	private String nome;
+	private int votos;
+	private int numero;
+	
+	public Candidato() {
+	}
+
+	public Candidato(String nome, int numero) {
+		this.nome = nome;
+		this.numero = numero;
+  }
+  
+	public String getNome() {
+		return nome;
+	}
+
+	public int getVotos() {
+		return votos;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+	
+	public int addVotos() {
+		return votos++;
+	}
+	
+	public String toString() { 
+		return "\nNome: "+ nome + "\nNúmero: "+ numero + "\nVotos: "+ votos;
+	}
+	
+	public static Candidato localizarCandidato(Candidato[] vetor, int numero) {
+        for (Candidato candidato : vetor) {
+            if (candidato.getNumero() == numero) {
+                return candidato;
+            }
+        } 
+        return null; 
+	}
+} 
